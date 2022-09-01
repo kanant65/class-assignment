238832967fb7a77c5a9d2efd5763f1883ef0c2d5
kanant65 <86822605+kanant65@users.noreply.github.com>
2022-09-01 13:20:39 +0530

    Add files via upload

---
+++ TestEmployee.java
@@ -0,0 +1,28 @@
+package com.oops.bll;
+
+public class TestEmployee 
+{
+
+	public static void main(String[] args)// main
+	{
+	      // Test constructor and toString()
+	      Employee e1 = new Employee(8, "Peter", "Tan", 2500);
+	      System.out.println(e1);  // toString();
+
+	      // Test Setters and Getters
+	      e1.setSalary(999);
+	      System.out.println(e1);  // toString();
+	      System.out.println("id is: " + e1.getId());
+	      System.out.println("firstname is: " + e1.getFirstName());
+	      System.out.println("lastname is: " + e1.getLastName());
+	      System.out.println("salary is: " + e1.getSalary());
+
+	      System.out.println("name is: " + e1.getName());
+	      System.out.println("annual salary is: " + e1.getAnnualSalary()); // Test method
+
+	      // Test raiseSalary()
+	      System.out.println(e1.getraiseSalary(10));
+	      System.out.println(e1);
+	   }
+
+}
